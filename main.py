d5788619a543228b2ec2450eaefa0909575b310a
Sam <sa235@live.ru>
2021-05-06 14:17:25 +0500

    Implement simple test

--- main.py
+++ main.py
@@ -1,16 +1,116 @@
-# This is a sample Python script.
+from random import choice
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    verbs = ['love']
+    pronounses = ['I', 'You', 'We', 'They', 'He', 'She']
+    times = ['Present simple', 'Past simple', 'Future simple']
+    times = ['Present simple', 'Past simple', 'Future simple']
+    forms = ['Positive', 'Negative', 'Question']
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    matrix = []
+
+    for form in forms:
+        if form == 'Positive':
+            for time in times:
+                if time == 'Present simple':
+                    for pronounse in pronounses:
+                        for verb in verbs:
+                            if pronounse == 'He' or pronounse == 'She':
+                                example = '{} {}s'.format(pronounse, verb)
+                                matrix.append(('Present + (' + pronounse + ' ' + verb + ')', example))
+                            else:
+                                example = '{} {}'.format(pronounse, verb)
+                                matrix.append(('Present + (' + pronounse + ' ' + verb + ')', example))
+                elif time == 'Past simple':
+                    for pronounse in pronounses:
+                        for verb in verbs:
+                            example = '{} {}d'.format(pronounse, verb)
+                            matrix.append(('Past + (' + pronounse + ' ' + verb + ')', example))
+                elif time == 'Future simple':
+                    for pronounse in pronounses:
+                        for verb in verbs:
+                            example = '{} will {}'.format(pronounse, verb)
+                            matrix.append(('Future + (' + pronounse + ' ' + verb + ')', example))
+                else:
+                    print('ERROR: time')
+
+        elif form == 'Negative':
+            for time in times:
+                if time == 'Present simple':
+                    for pronounse in pronounses:
+                        for verb in verbs:
+                            if pronounse == 'He' or pronounse == 'She':
+                                example = '{} doesen`t {}'.format(pronounse, verb)
+                                matrix.append(('Present - (' + pronounse + ' ' + verb + ')', example))
+                            else:
+                                example = '{} don`t {}'.format(pronounse, verb)
+                                matrix.append(('Present - (' + pronounse + ' ' + verb + ')', example))
+                elif time == 'Past simple':
+                    for pronounse in pronounses:
+                        for verb in verbs:
+                            example = '{} didn`t {}'.format(pronounse, verb)
+                            matrix.append(('Past - (' + pronounse + ' ' + verb + ')', example))
+                elif time == 'Future simple':
+                    for pronounse in pronounses:
+                        for verb in verbs:
+                            example = '{} will not {}'.format(pronounse, verb)
+                            matrix.append(('Future - (' + pronounse + ' ' + verb + ')', example))
+                else:
+                    print('ERROR: time')
+        elif form == 'Question':
+            for time in times:
+                if time == 'Present simple':
+                    for pronounse in pronounses:
+                        for verb in verbs:
+                            if pronounse == 'He' or pronounse == 'She':
+                                example = 'Does {} {}'.format(pronounse, verb)
+                                matrix.append(('Present ? (' + pronounse + ' ' + verb + ')', example))
+                            else:
+                                example = 'Do {} {}'.format(pronounse, verb)
+                                matrix.append(('Present ? (' + pronounse + ' ' + verb + ')', example))
+                elif time == 'Past simple':
+                    for pronounse in pronounses:
+                        for verb in verbs:
+                            example = 'Did {} {}?'.format(pronounse, verb)
+                            matrix.append(('Past ? (' + pronounse + ' ' + verb + ')', example))
+                elif time == 'Future simple':
+                    for pronounse in pronounses:
+                        for verb in verbs:
+                            example = 'Will {} {}?'.format(pronounse, verb)
+                            matrix.append(('Future ? (' + pronounse + ' ' + verb + ')', example))
+                else:
+                    print('ERROR: time')
+        else:
+            print('ERROR: form')
+
+true_answer = 0
+false_answer = 0
+
+for i in range(1, 53):
+    variant = choice(matrix)
+    ask = input(variant[0] + ': ')
+    if ask.lower() == variant[1].lower():
+        true_answer += 1
+        print('Правильно! ', true_answer)
+    else:
+        false_answer += 1
+        print('Ошибка! ', true_answer)
+        print('Правильный ответ: ', variant[1])
+
+print('true_answer ', true_answer)
+print('false_answer ', false_answer)
+
+# for ex in matrix.items():
+#     print(ex)
+
+
+
+
+
+
+
+
+
+
+
+
