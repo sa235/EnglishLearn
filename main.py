41f917d03ba7fdf5744b8eee3a2c8a9c5f29e773
Sam <sa235@live.ru>
2021-05-11 00:51:53 +0500

    Add irregular vorbs to test, refactoring

--- main.py
+++ main.py
@@ -1,94 +1,94 @@
 from random import shuffle
+from random import choice
+
+verbs = ['love', 'watch', ['see', 'saw'], ['go', 'went']]
+
+
+def get_random_verb():
+    verb = choice(verbs)
+    return verb
+
+
+def get_verb_form(verb, formkey):
+    if isinstance(verb, list):
+        return verb[formkey]
+    else:
+        return verb
+
+
+def verb_format(verb):
+    if isinstance(verb, list):
+        return '{} ({})'.format(verb[0], verb[1])
+    else:
+        return verb
+
+
+def get_correct_answer(form, time, pronounse, verb):
+    if form == '+':
+        if time == 'Present simple':
+            if pronounse == 'He' or pronounse == 'She':
+                return '{} {}s'.format(pronounse, get_verb_form(verb, 0))
+            else:
+                return '{} {}'.format(pronounse, get_verb_form(verb, 0))
+        elif time == 'Past simple':
+            if isinstance(verb, list):
+                return '{} {}'.format(pronounse, get_verb_form(verb, 1))
+            else:
+                return '{} {}d'.format(pronounse, get_verb_form(verb, 0))
+        elif time == 'Future simple':
+            return '{} will {}'.format(pronounse, get_verb_form(verb, 0))
+        else:
+            print('ERROR: time')
+
+    elif form == '-':
+        if time == 'Present simple':
+            if pronounse == 'He' or pronounse == 'She':
+                return '{} doesn`t {}'.format(pronounse, get_verb_form(verb, 0))
+            else:
+                return '{} don`t {}'.format(pronounse, get_verb_form(verb, 0))
+        elif time == 'Past simple':
+            return '{} didn`t {}'.format(pronounse, get_verb_form(verb, 0))
+        elif time == 'Future simple':
+            return '{} will not {}'.format(pronounse, get_verb_form(verb, 0))
+        else:
+            print('ERROR: time')
+    elif form == '?':
+        if time == 'Present simple':
+            if pronounse == 'He' or pronounse == 'She':
+                return 'Does {} {}?'.format(pronounse, get_verb_form(verb, 0))
+            else:
+                return 'Do {} {}?'.format(pronounse.lower(), get_verb_form(verb, 0))
+        elif time == 'Past simple':
+            return 'Did {} {}?'.format(pronounse.lower(), get_verb_form(verb, 0))
+        elif time == 'Future simple':
+            return 'Will {} {}?'.format(pronounse.lower(), get_verb_form(verb, 0))
+        else:
+            print('ERROR: time')
+    else:
+        print('ERROR: form')
+
 
 if __name__ == '__main__':
-    verbs = ['love']
+
     pronounses = ['I', 'You', 'We', 'They', 'He', 'She']
     times = ['Present simple', 'Past simple', 'Future simple']
-    times = ['Present simple', 'Past simple', 'Future simple']
-    forms = ['Positive', 'Negative', 'Question']
+    forms = ['+', '-', '?']
 
     questions = []
 
     for form in forms:
-        if form == 'Positive':
-            for time in times:
-                if time == 'Present simple':
-                    for pronounse in pronounses:
-                        for verb in verbs:
-                            if pronounse == 'He' or pronounse == 'She':
-                                example = '{} {}s'.format(pronounse, verb)
-                                questions.append(('Present + (' + pronounse + ' ' + verb + ')', example))
-                            else:
-                                example = '{} {}'.format(pronounse, verb)
-                                questions.append(('Present + (' + pronounse + ' ' + verb + ')', example))
-                elif time == 'Past simple':
-                    for pronounse in pronounses:
-                        for verb in verbs:
-                            example = '{} {}d'.format(pronounse, verb)
-                            questions.append(('Past + (' + pronounse + ' ' + verb + ')', example))
-                elif time == 'Future simple':
-                    for pronounse in pronounses:
-                        for verb in verbs:
-                            example = '{} will {}'.format(pronounse, verb)
-                            questions.append(('Future + (' + pronounse + ' ' + verb + ')', example))
-                else:
-                    print('ERROR: time')
-
-        elif form == 'Negative':
-            for time in times:
-                if time == 'Present simple':
-                    for pronounse in pronounses:
-                        for verb in verbs:
-                            if pronounse == 'He' or pronounse == 'She':
-                                example = '{} doesn`t {}'.format(pronounse, verb)
-                                questions.append(('Present - (' + pronounse + ' ' + verb + ')', example))
-                            else:
-                                example = '{} don`t {}'.format(pronounse, verb)
-                                questions.append(('Present - (' + pronounse + ' ' + verb + ')', example))
-                elif time == 'Past simple':
-                    for pronounse in pronounses:
-                        for verb in verbs:
-                            example = '{} didn`t {}'.format(pronounse, verb)
-                            questions.append(('Past - (' + pronounse + ' ' + verb + ')', example))
-                elif time == 'Future simple':
-                    for pronounse in pronounses:
-                        for verb in verbs:
-                            example = '{} will not {}'.format(pronounse, verb)
-                            questions.append(('Future - (' + pronounse + ' ' + verb + ')', example))
-                else:
-                    print('ERROR: time')
-        elif form == 'Question':
-            for time in times:
-                if time == 'Present simple':
-                    for pronounse in pronounses:
-                        for verb in verbs:
-                            if pronounse == 'He' or pronounse == 'She':
-                                example = 'Does {} {}?'.format(pronounse, verb)
-                                questions.append(('Present ? (' + pronounse + ' ' + verb + ')', example))
-                            else:
-                                example = 'Do {} {}?'.format(pronounse, verb)
-                                questions.append(('Present ? (' + pronounse + ' ' + verb + ')', example))
-                elif time == 'Past simple':
-                    for pronounse in pronounses:
-                        for verb in verbs:
-                            example = 'Did {} {}?'.format(pronounse, verb)
-                            questions.append(('Past ? (' + pronounse + ' ' + verb + ')', example))
-                elif time == 'Future simple':
-                    for pronounse in pronounses:
-                        for verb in verbs:
-                            example = 'Will {} {}?'.format(pronounse, verb)
-                            questions.append(('Future ? (' + pronounse + ' ' + verb + ')', example))
-                else:
-                    print('ERROR: time')
-        else:
-            print('ERROR: form')
-
-true_answer = 0
-false_answer = 0
+        for time in times:
+            for pronounse in pronounses:
+                verb = choice(verbs)
+                correct_answer = get_correct_answer(form, time, pronounse, verb)
+                questions.append(('{} {} | {} {}'.format(time, form, pronounse, verb_format(verb)), correct_answer))
 
 shuffle(questions)
 
-# for ex in matrix:
+true_answer = 0
+false_answer = 0
+#
+# for ex in questions:
 #     print(ex)
 
 i = 0
@@ -107,7 +107,6 @@ for question in questions:
     i += 1
     print('Прогресс: {}..{} True: {}  False: {}'.format(i, ml, true_answer, false_answer))
 
-
 print('true_answer ', true_answer)
 print('false_answer ', false_answer)
 
@@ -117,15 +116,3 @@ print('false_answer ', false_answer)
 # Прогресс: 54..54 True: 50  False: 4
 # true_answer  50
 # false_answer  4
-
-
-
-
-
-
-
-
-
-
-
-
